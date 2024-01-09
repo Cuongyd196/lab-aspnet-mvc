37adc019f0ec52228fdc71cf1cedff8838ae0b1b
Cuongyd196 <Cuongyd196@gmail.com>
2024-01-09 22:33:11 +0700

    #Init

---
+++ App01/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult PhepTinh()
+        {
+            ViewBag.Message = "Phép tính";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PhepTinh(int soA, int soB, string pheptinh)
+        {
+
+            int result = 0;
+            switch(pheptinh)
+            {
+                case "+":
+                    result = soA + soB;
+                    break;
+                case "-":
+                    result = soA - soB;
+                    break;
+                case "*":
+                    result = soA * soB;
+                    break;
+                case "/":
+                    result = soA / soB;
+                    break;
+            }    
+            ViewBag.result = "Kết quả phép tính là: " +  result;
+            return View();
+        }
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Mô tả source code
+- [App01 - Khởi tạo dự án, phép tính 2 số](https://github.com/Cuongyd196/lab-aspnet-mvc/tree/main/app01)
+
+# Đọc log WriteLine
+System.Diagnostics.Debug.WriteLine("");
+
+
